72c2d388b2c046b6454521e6af1d8238f4f6dcd5
Ekam Lally <Elally7172@conestogac.on.ca>
2022-11-23 19:48:15 -0500

    finished isValidRectangle function - Ekam Lally

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -4,7 +4,17 @@
 
 bool isValidRectangle(PAIRS a, PAIRS b, PAIRS c, PAIRS d) // e.l - created function
 {
-	
+	if (getLengthBetweenPoints(a, b) == getLengthBetweenPoints(c, d) && getLengthBetweenPoints(a, c) == getLengthBetweenPoints(b, d) && getLengthBetweenPoints(a, b) != getLengthBetweenPoints(a, c))
+	{
+		printf("Is a valid rectangle");
+		return true;
+	}
+		
+	else
+	{
+		printf("Is not a valid rectangle");
+		return false;
+	}
 }
 
 int getLengthBetweenPoints(PAIRS x, PAIRS y)
