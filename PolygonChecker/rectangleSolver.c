a34e18631f82ef1ed951c6973e42c094fe2254b3
Ekam Lally <Elally7172@conestogac.on.ca>
2022-11-23 19:28:11 -0500

    started isValidRectangle function - Ekam Lally

---
+++ PolygonChecker/rectangleSolver.c
@@ -0,0 +1,8 @@
+#include "rectangleSolver.h"
+#include<stdio.h>
+#include<math.h>
+
+bool isValidRectangle(PAIRS a, PAIRS b, PAIRS c, PAIRS d) // e.l - created function
+{
+	
+}
---
+++ PolygonChecker/rectangleSolver.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdbool.h> // e.l
+
+typedef struct pairs {
+	int x;
+	int y;
+}PAIRS;
+
+bool isValidRectangle(PAIRS a, PAIRS b, PAIRS c, PAIRS d);  // e.l 
